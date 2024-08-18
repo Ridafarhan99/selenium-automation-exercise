bb3ec29d521314394feaf55f5f53de66ed9092a1
Rida Farhan <42339599+Ridafarhan99@users.noreply.github.com>
2024-08-19 01:40:33 +0530

    loadconfig & launchapp added in baseclass

---
+++ src/main/java/com/mystore/base/BaseClass.java
@@ -0,0 +1,47 @@
+package com.mystore.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class BaseClass {
+    public static Properties props = new Properties();
+    public static WebDriver driver;
+
+    @BeforeSuite
+    public void loadConfiguration() {
+        try{
+            System.out.println("Loading configuration...");
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + "Configuration/config.properties");
+            props.load(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void launchApp(String browserName) {
+        // String browserName = prop.getProperty("browser");
+        if (browserName.equalsIgnoreCase("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            // Set Browser to ThreadLocalMap
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("FireFox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("IE")) {
+            WebDriverManager.iedriver().setup();
+            driver = new InternetExplorerDriver();
+        }
+
+    }
+}
